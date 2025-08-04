3de5e959c6e6f9d5bc6e2d53dfe3c8cee0e32a1e
NoinBlake336 <josechocobar336@gmail.com>
2025-08-04 16:00:54 -0300

    04/08/2025

--- prisma/migrations/20250804134341_unique_decription/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[description]` on the table `EProduct` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "EProduct_description_key" ON "EProduct"("description");
